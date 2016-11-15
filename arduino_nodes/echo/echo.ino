f4958e5ccf73a5f5af77290d829b5653587ffe5e
Benjamin Schaff <benjamin.schaff@maine.edu>
2016-11-14 19:03:11 -0500

    formatting and comments

--- arduino_nodes/echo/echo.ino
+++ arduino_nodes/echo/echo.ino
@@ -1,12 +1,12 @@
 /*
- * Simple test node, echos and recieved messages back to sender.  Prints to serial for debug
+ * Simple test node, echos and recieved messages back to sender.	Prints to serial for debug
  */
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 
 
-// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+// Set up nRF24L01 radio on SPI bus plus pins 9 & 10 for CE and CSN
 RF24 radio(9, 10);
 
 // Addresses of the 2 nodes. 5 bytes each
@@ -15,50 +15,50 @@ const uint64_t node_addr = 0xF0F0F0F0A1;
 
 void setup(void)
 {
-  Serial.begin(9600);
-  Serial.print("\n\rRF24 echo test\n\r");
+	Serial.begin(9600);
+	Serial.print("\n\rRF24 echo test\n\r");
 
-  radio.begin();
-  radio.setRetries(15, 15); //15 retries after 15*250us each
-  radio.setPayloadSize(8); // Reduce the payload size.  seems to improve reliability
+	radio.begin();
+	radio.setRetries(15, 15); //15 retries after 15*250us each
+	radio.setPayloadSize(8); // Reduce the payload size.	seems to improve reliability
 
-  radio.openWritingPipe(node_addr); //we are remote node
-  radio.openReadingPipe(1, root_addr); //read from root
+	radio.openWritingPipe(node_addr); //we are remote node
+	radio.openReadingPipe(1, root_addr); //read from root
 
-  radio.startListening();
+	radio.startListening();
 
-  // print config for debug
-  radio.printDetails();
+	// print config for debug
+	radio.printDetails();
 }
 
 void loop(void)
 {
-  if (radio.available())  // if there is data ready
-  {
-    unsigned long payload;
-    bool done = false;
-    while (!done)
-    {
-      // Fetch the payload, and see if this was the last one.
-      done = radio.read( &payload, sizeof(unsigned long) );
+	if (radio.available())	// if there is data ready
+	{
+		unsigned long payload;
+		bool done = false;
+		while (!done)
+		{
+			// Fetch the payload, and see if this was the last one.
+			done = radio.read( &payload, sizeof(unsigned long) );
 
-      //print to serial for debug
-      Serial.print("Got: ");
-      Serial.print(payload);
-      
-      // wait for other to switch modes
-      delay(20);
-    }
+			//print to serial for debug
+			Serial.print("Got: ");
+			Serial.print(payload);
+			
+			// wait for other to switch modes
+			delay(20);
+		}
 
-    // stop rx before tx
-    radio.stopListening();
+		// stop rx before tx
+		radio.stopListening();
 
-    // echo payload
-    radio.write( &payload, sizeof(unsigned long) );
-    Serial.print(" Sent response.\n\r");
+		// echo payload
+		radio.write( &payload, sizeof(unsigned long) );
+		Serial.print(" Sent response.\n\r");
 
-    // back to rx mode
-    radio.startListening();
-  }
+		// back to rx mode
+		radio.startListening();
+	}
 }
 
--- arduino_nodes/tx_test/tx_test.ino
+++ arduino_nodes/tx_test/tx_test.ino
@@ -1,10 +1,13 @@
+/*
+ * Simple transmit test, sends current time and waits for response.
+ */	
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
 
-// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+// Set up nRF24L01 radio on SPI bus plus pins 9 & 10 for CE and CSN
 RF24 radio(9, 10);
 
 // Addresses of the 2 nodes. 5 bytes each
@@ -13,72 +16,71 @@ const uint64_t node_addr = 0xF0F0F0F0A1;
 
 void setup(void)
 {
-  Serial.begin(57600);
-  printf_begin();
-  printf("\n\rRF24/examples/pingpair/\n\r");
+	Serial.begin(57600);
+	printf_begin();
+	printf("\n\rRF24/examples/pingpair/\n\r");
 
 
-  // Setup and configure rf radio
-  radio.begin();
+	// Setup and configure rf radio
+	radio.begin();
 
 
-  radio.setRetries(15, 15); // retries 15 times after 15*250us each
+	radio.setRetries(15, 15); // retries 15 times after 15*250us each
 
-  // optionally, reduce the payload size.  seems to improve reliability
-  radio.setPayloadSize(8);
+	// optionally, reduce the payload size.	seems to improve reliability
+	radio.setPayloadSize(8);
 
-  // Open pipes to other nodes for communication
-  radio.openWritingPipe(root_addr); //we are root, write from here
-  radio.openReadingPipe(1, node_addr); //read from node
+	// Open pipes to other nodes for communication
+	radio.openWritingPipe(root_addr); //we are root, write from here
+	radio.openReadingPipe(1, node_addr); //read from node
 
-  radio.startListening();
+	radio.startListening();
 
-  // print config for debug
-  radio.printDetails();
+	// print config for debug
+	radio.printDetails();
 }
 
 void loop(void)
 {
-  // First, stop listening so we can talk.
-  radio.stopListening();
+	// End rx mode before tx.
+	radio.stopListening();
 
-  // Take the time, and send it.  This will block until complete
-  unsigned long time = millis();
-  Serial.print("Now sending ");
-  Serial.print(time);
-  bool ok = radio.write( &time, sizeof(unsigned long) );
+	// Send current time
+	unsigned long time = millis();
+	Serial.print("Now sending ");
+	Serial.print(time);
+	bool success = radio.write( &time, sizeof(unsigned long) );
 
-  if (ok)
-    Serial.print(" ok...");
-  else
-    Serial.print(" failed.\n\r");
+	if (success) {
+		Serial.print(" success...");
+	} else {
+		Serial.print(" failed.\n\r");
+	}
 
-  // Now, continue listening
-  radio.startListening();
+	// Resume rx
+	radio.startListening();
 
-  // Wait here until we get a response, or timeout (250ms)
-  unsigned long started_waiting_at = millis();
-  bool timeout = false;
-  while ( ! radio.available() && ! timeout )
-    if (millis() - started_waiting_at > 200 )
-      timeout = true;
+	// Wait for response, or timeout (250ms)
+	unsigned long started_waiting_at = millis();
+	bool timeout = false;
+	while ( ! radio.available() && ! timeout ) {
+		if (millis() - started_waiting_at > 200 ) {
+			timeout = true;
+		}
+	}
 
-  // Describe the results
-  if ( timeout )
-  {
-      Serial.print("Failed, response timed out.\r\n");
-      delay(50);
-    }
-  else
-  {
-  
-      unsigned long payload;
-      radio.read( &payload, sizeof(unsigned long) );
-  
-      // print revieved message
-      Serial.print("Got response ");
-      Serial.println(payload);
-  
-      delay(2000);
-    }
+	// Print results
+	if ( timeout ) {
+			Serial.print("Failed, response timed out.\r\n");
+			delay(50);
+	} else {
+		unsigned long payload;
+		radio.read( &payload, sizeof(unsigned long) );
+
+		// print revieved message
+		Serial.print("Got response ");
+		Serial.println(payload);
+
+		delay(2000);
+	}
 }
